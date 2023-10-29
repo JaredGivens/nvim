0f847fd58099a9523ac9a347df348bfe4d551c20
Jared <jareddocg@gmail.com>
2023-10-29 00:02:21 -0700

    formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,20 +1,21 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
+    lsp_zero.buffer_autoformat()
 end)
 
 local cmp = require('cmp')
 local cmp_action = lsp_zero.cmp_action()
 cmp.setup({
     sources = {
-        {name = 'nvim_lsp'},
+        { name = 'nvim_lsp' },
     },
     mapping = {
-      ['<Tab>'] = cmp_action.tab_complete(),
-      ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
     },
 })
 
